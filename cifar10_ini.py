a55031ac86658b79da00c509fd6836799ed90231
papy <e-papy@nifty.com>
2018-08-30 21:45:56 +0900

    Add files via upload

---
+++ cifar10_ini.py
@@ -0,0 +1,20 @@
+# File name of training data
+# (訓練データのファイル名)
+TRAIN_DATA_FILE = 'train_data.bin'
+
+# File name of evaluation data
+# (評価データのファイル名)
+EVAL_DATA_FILE  = 'eval_data.bin'
+
+# Number of classes
+# (クラス数)
+NUM_CLASSES = 15
+
+# Number of training data
+# (訓練データ数)
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 3896
+
+# umber of evaluation data
+# (評価データ数)
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL  = 423
+
